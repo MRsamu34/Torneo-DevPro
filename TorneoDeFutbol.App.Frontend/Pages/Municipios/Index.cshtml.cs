995ce49da6a8232641cc3882671bf6e34b244e42
May-da <90736804+May-da@users.noreply.github.com>
2021-10-03 21:53:11 -0500

    Pages arbitros y municipios

---
+++ TorneoDeFutbol.App.Frontend/Pages/Municipios/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoDeFutbol.App.Dominio;
+using TorneoDeFutbol.App.Persistencia;
+
+namespace Torneo.App.Frontend.Pages.Municipios
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IRepositorioMunicipio _repoMunicipio;
+        public IEnumerable<Municipio> municipios {get; set;}
+        public IndexModel(IRepositorioMunicipio repoMunicipio)
+        {
+            _repoMunicipio = repoMunicipio;
+        }
+        public void OnGet()
+        {
+            municipios = _repoMunicipio.GetAllMunicipio();
+        }
+    }
+}
