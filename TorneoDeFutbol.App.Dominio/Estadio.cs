8acd01c1393fa1f43707f89496927a0333f2467f
MRsamu34 <samusoccer34@gmail.com>
2021-10-10 18:36:13 -0500

    todos los pages

--- TorneoDeFutbol.App.Dominio/Estadio.cs
+++ TorneoDeFutbol.App.Dominio/Estadio.cs
@@ -5,13 +5,11 @@ namespace TorneoDeFutbol.App.Dominio
     /// <summary>Class <c>Estadio</c>
     /// </summary>   
     public class Estadio
-    {
-         
+    {       
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Direccion {get; set;}
         public Municipio Municipio {get; set;}
         public Partido Partido {get; set;}
-
     }
 }
